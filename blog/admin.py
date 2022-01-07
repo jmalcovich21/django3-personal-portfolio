0186be2afa7983c3b23ace30859ca112576a0d17
John Malcovich <jmalcovich21@outlook.com>
2022-01-07 18:35:58 -0500

    First commit!

---
+++ blog/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Blog
+
+# Register your models here.
+
+admin.site.register(Blog) # los modelos que quiero que aparezcan en la interfaz de administracion
